3419bdcbbca0de4b2a157bd6656e95f21002a901
AzizaAzhibekova <115431738+AzizaAzhibekova@users.noreply.github.com>
2023-05-04 19:04:57 +0300

    Added product comparison functionality.

---
+++ src/main/java/us/peaksoft/gadgetarium/service/ComparisonService.java
@@ -0,0 +1,17 @@
+package us.peaksoft.gadgetarium.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import us.peaksoft.gadgetarium.dto.ComparisonResponse;
+import us.peaksoft.gadgetarium.enums.Subcat;
+
+public interface ComparisonService {
+    ResponseEntity<String> addToComparison(@RequestBody Long id);
+
+    List<ComparisonResponse> getComparisonListBySubCategory(@PathVariable Subcat subcat);
+
+    ResponseEntity<String> clearList();
+}
