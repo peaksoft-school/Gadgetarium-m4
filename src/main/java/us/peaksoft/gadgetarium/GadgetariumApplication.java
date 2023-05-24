fe55cb2cef4b61b12897a0a5c73f90d5e58212d1
Bektur <bectur97@gmail.com>
2023-05-24 22:28:26 +0300

    resolve 1

--- src/main/java/us/peaksoft/gadgetarium/GadgetariumApplication.java
+++ src/main/java/us/peaksoft/gadgetarium/GadgetariumApplication.java
@@ -3,6 +3,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import us.peaksoft.gadgetarium.dto.ContactRequest;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 @Controller
 @SpringBootApplication
@@ -11,6 +17,12 @@ public class GadgetariumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GadgetariumApplication.class, args);
 		System.out.println("Welcome colleagues, project name is Gadgetarium");
+//		ContactRequest contact = new ContactRequest();
+//		contact.setNumber("8393939393993");
+//		contact.setUsername("king kobra");
+//		contact.setName("aiadai");
+//		contact.setMessage("hello");
+//		contact.setEmail("jibek.82@gmail.com");
 	}
 	@GetMapping("/")
 	public String greetingPage(){
